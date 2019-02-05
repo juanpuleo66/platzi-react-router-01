45f625d97bd0439ef066d197bf33f895bd645f7f
juanpuleo66 <juanpuleo@gmail.com>
2019-02-04 23:32:07 -0500

    Creacion del Proyecto

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = (env) => {
   return {
 
     entry: {
-      "home": path.resolve(__dirname, 'src/entries/home.js'),
-      "redux": path.resolve(__dirname, 'src/entries/redux.js'),
+      "add": path.resolve(__dirname, 'src/entries/add.js'),
+      // "redux": path.resolve(__dirname, 'src/entries/redux.js'),
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
